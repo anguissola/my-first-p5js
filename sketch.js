2da27281b0c1f71e72495a2dd68f05f60ebd5ada
ANDY MILLS <SM21BM@FSU.EDU>
2023-09-11 11:36:08 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  fill("red");
+  noStroke();
 }
 
 function draw() {
-  background(125);
+  background("pink");
+  rect(200, 200, 100, 100);
+  ellipse(30, 30, 30);
 }
+
+
